279781c00950924edc7e60dc12269b9071c89b80
ClementPla <clement.playout@polymtl.ca>
2019-04-10 13:57:14 +0900

    Correct slerp

--- scripts/interpolation.py
+++ scripts/interpolation.py
@@ -12,6 +12,10 @@ def deep_interpolation(arr1, arr2, nb_frames, model, optimize_z=True, spherical=
 
     z1 = model.encoder(tens1)
     z2 = model.encoder(tens2)
+
+    z1_norm = torch.norm(z1)
+    z2_norm = torch.norm(z2)
+
     if optimize_z:
         z1 = reverse_z(model.decoder, tens1, z_size, gpu, initial_z=z1, **kwargs)
         z2 = reverse_z(model.decoder, tens2, z_size, gpu, initial_z=z2, **kwargs)
@@ -20,13 +24,17 @@ def deep_interpolation(arr1, arr2, nb_frames, model, optimize_z=True, spherical=
 
     if spherical:
         theta = get_angle(z1, z2)
+        z1 = z1/z1_norm
+        z2 = z2 / z2_norm
+        z_mean_norm = (z1_norm+z2_norm)/2
 
-    for f in range(nb_frames):
-        f = f / nb_frames
+    time = np.linspace(0, 1, nb_frames+1)[1:]
+    for t in range(time):
         if spherical:
-            z = z1 * torch.sin((1 - f) * theta) / torch.sin(theta) + z2 * torch.sin(f * theta) / torch.sin(theta)
+            z = z1 * torch.sin((1 - t) * theta) / torch.sin(theta) + z2 * torch.sin(t * theta) / torch.sin(theta)
+            z *= z_mean_norm
         else:
-            z = z1*(1-f)+z2*f
+            z = z1*(1-t)+z2*t
 
         tens = model.decoder(z)
 
