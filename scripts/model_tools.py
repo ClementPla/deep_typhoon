866f16e154f2844eaa9189853c056851ff6a500d
ClementPla <clement.playout@polymtl.ca>
2019-04-17 15:29:46 +0900

    Bug fix

--- scripts/model_tools.py
+++ scripts/model_tools.py
@@ -16,6 +16,7 @@ def forward(model, arr, b=8, gpu=0, only_decode=True):
 def decoding(model, z, b=8, gpu=0):
     gen = batch_gen(z, batch_size=b, vector=True)
     output = []
+    model.eval()
     for arr in gen:
         tens_out = model.decoder(convert_numpy_to_tensor(arr, gpu, vector=True))
         output.append(convert_tensor_to_numpy(tens_out))
@@ -26,6 +27,7 @@ def encoding(model, arr, b=8, gpu=0, optimize_z=False, **kwargs):
     gen = batch_gen(arr, batch_size=b)
     z_size = model.z_size
     output = []
+    model.eval()
     for arr in gen:
         goal_tens = convert_numpy_to_tensor(arr, gpu)
         tens_out = model.encoder(goal_tens)
