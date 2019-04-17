f59339a6abe9dc3e7575ac7a06f69d1cc2f563a6
ClementPla <clement.playout@polymtl.ca>
2019-04-17 16:09:11 +0900

    bug fix

--- scripts/model_tools.py
+++ scripts/model_tools.py
@@ -8,7 +8,7 @@ def forward(model, input_imgs, b=8, gpu=0, only_decode=True):
     output = []
     model.eval()
     for arr in gen:
-        tens_out = model.encoder(convert_numpy_to_tensor(arr, gpu), only_decode=only_decode)
+        tens_out = model.encoder(convert_numpy_to_tensor(arr, gpu))
         output.append(convert_tensor_to_numpy(model.decoder(tens_out)))
     return np.asarray(output)
 
