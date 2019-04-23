2a57006fd579b92868434400a87143ac7ad79c2e
ClementPla <clement.playout@polymtl.ca>
2019-04-23 19:33:23 +0900

    Add linear interpolation values for NaN

--- utils/temporal_tools.py
+++ utils/temporal_tools.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from scripts.interpolation import linear_interpolation
 
 def add_time_interval(df, data_col='z_space'):
     """
@@ -18,7 +18,7 @@ def add_time_interval(df, data_col='z_space'):
         l = []
         previous_z = []
         m_seq = np.asarray(df.loc[seq]['m'])
-        contains_NaN = np.any(m_seq)
+        contains_NaN = np.any(~m_seq)
         if contains_NaN:
             z_space_seq = np.asarray(df.loc[seq][data_col])
         else:
@@ -54,3 +54,41 @@ def get_sequence_max_length(df):
         if seq_length>max_length:
             max_length = seq_length
     return max_length
+
+
+def interpolate_nan_values(df, data_col='z_space'):
+    """
+    Interpolate NaN cells with previous and next neighbors
+    :param df:
+    :param data_col:
+    :return:
+    """
+    pd.options.mode.chained_assignment = None
+    df['m'] = ~pd.isna(df['z_space'])
+    sequences = np.unique(df.index.get_level_values(0))
+    new_data_space_full = []
+    for seq in sequences:
+        m_seq = np.asarray(df.loc[seq]['m'])
+        contains_NaN = np.any(~m_seq)
+        z_space_seq = list(df.loc[seq][data_col])
+        if contains_NaN:
+            for i in np.arange(len(m_seq)):
+                if isinstance(z_space_seq[i], float):
+                    prev = z_space_seq[i - 1]
+                    found_next = False
+                    j = 0
+                    while not found_next:
+                        j += 1
+                        found_next = not isinstance(z_space_seq[i + j], float)
+
+                    next_ = z_space_seq[i + j]
+                    interpolated = linear_interpolation(prev, next_, nb_frames=j)
+                    for h in range(j):
+                        z_space_seq[i + h] = interpolated[h]
+        new_data_space_full += z_space_seq
+
+    df[data_col] = new_data_space_full
+    pd.options.mode.chained_assignment = 'warn'
+    return df
+
+
