2a47343b1a2b4525e550efb685a227ee610fdcf5
ClementPla <clement.playout@polymtl.ca>
2019-05-02 12:15:25 +0900

    Add imputation function

--- networks/RNN.py
+++ networks/RNN.py
@@ -1,6 +1,8 @@
 from .abstract_network import AbstractNet
 from torch import nn
-
+from torch.nn.parameter import Parameter
+import torch
+from .. import functional as F
 
 class LSTMNet(AbstractNet):
     def __init__(self,
@@ -14,11 +16,13 @@ class LSTMNet(AbstractNet):
                  cell_type='lstm',
                  dropout=0.5,
                  gpu=0,
-                 non_linearity='tanh'):
+                 non_linearity='tanh', impute_data = False):
 
         super(LSTMNet, self).__init__(checkpoint, gpu)
 
         self.cell_type = cell_type
+        self.impute_data = impute_data
+
         if cell_type == 'lstm':
             self.inner_model = nn.LSTM(input_dimensions, hidden_size,
                                        dropout=dropout, num_layers=num_layers,
@@ -43,9 +47,20 @@ class LSTMNet(AbstractNet):
                                        batch_first=batch_first,
                                        nonlinearity=non_linearity)
 
+        if self.impute_data:
+            self.imputation_activation = nn.ReLu()
+            self.imputation_w = Parameter(torch.Tensor(input_dimensions))
+            self.imputation_b = Parameter(torch.Tensor(input_dimensions))
+
     def forward(self, x):
         lstm_out = self.inner_model(x)[0]
         out = self.output_model(lstm_out)[0]
         return out
+
+    def get_imputation_weight(self, l):
+        lin = F.linear(l, torch.diag(self.imputation_w), self.imputation_b)
+        return torch.exp(-1*self.imputation_activation(lin))
+
+
 
 
