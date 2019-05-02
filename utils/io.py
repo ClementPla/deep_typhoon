d22f25c1b393f585ab1d5cb28ce9591c75335570
ClementPla <clement.playout@polymtl.ca>
2019-05-02 10:14:10 +0900

    Bug fix

--- utils/io.py
+++ utils/io.py
@@ -9,7 +9,6 @@ def create_folder(folder_path):
 
 def get_most_recent_file(dirpath):
     files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(dirpath)) for f in fn]
-    files = [os.path.join(dirpath, f) for f in files]  # add path to each file
     files.sort(key=lambda x: os.path.getmtime(x))
     return os.path.join(dirpath, files[-1])
 
