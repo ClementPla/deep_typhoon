3fdd670e81f100f99494b81af829e38eef562a4b
ClementPla <clement.playout@polymtl.ca>
2019-05-02 10:08:09 +0900

    Add get_most_recent_files function

--- utils/io.py
+++ utils/io.py
@@ -1,9 +1,18 @@
-from os import path, makedirs
 import numpy as np
+import os
+
 
 def create_folder(folder_path):
-    if not path.exists(folder_path):
-        makedirs(folder_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
+def get_most_recent_file(dirpath):
+    os.chdir(dirpath)
+    files = filter(os.path.isfile, os.listdir(dirpath))
+    files = [os.path.join(dirpath, f) for f in files]  # add path to each file
+    files.sort(key=lambda x: os.path.getmtime(x))
+    return os.path.join(dirpath, files[-1])
 
 
 def save_numpy(arr, path):
