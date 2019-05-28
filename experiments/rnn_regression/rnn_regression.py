4b4f5d8b651c7b29ea09463d5819273f1042a15e
ClementPla <clement.playout@polymtl.ca>
2019-05-28 17:14:36 +0900

    Remove cudnn.benchmark setting

--- experiments/rnn_regression/rnn_regression.py
+++ experiments/rnn_regression/rnn_regression.py
@@ -35,7 +35,6 @@ class RNNRegressionTrainer():
         random.seed(seed)
 
         torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
 
